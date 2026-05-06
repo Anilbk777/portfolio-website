8f9f8d08248a18f34c6c71534e8003413110cb8f
Anilbk777 <anilbk775@gmail.com>
2026-05-06 16:14:10 +0545

    role updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,11 @@ const About = () => {
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
             <span className="text-white">I am a </span>
             <ReactTypingEffect
-              text={["Fullstack Developer", "AI/ML Engineer", "Coder"]}
+              text={[
+                "Fullstack Developer",
+                "AI/ML Engineer",
+                "Software Engineer",
+              ]}
               speed={100}
               eraseSpeed={50}
               typingDelay={500}
